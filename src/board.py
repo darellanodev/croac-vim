792ef761f14b4fbe4e34d831599633e4412ef6df
darellanodev <darellano.aliaga@gmail.com>
2025-11-13 21:29:32 +0100

    refactor: Extract check_bounds private method (closes #4)

--- src/board.py
+++ src/board.py
@@ -5,20 +5,18 @@ class Board:
     def __init__(self, layout):
         self.grid = [list(line) for line in layout]
 
-    def get_cell(self, x, y):
+    def _check_bounds(self, x, y):
         if x < 0 or y < 0:
             raise BoardIndexError("x and y positions must be zero or greater")
         if y >= len(self.grid):
             raise BoardIndexError("y position is out of range")
         if x >= len(self.grid[y]):
             raise BoardIndexError("x position is out of range")
+
+    def get_cell(self, x, y):
+        self._check_bounds(x, y)
         return self.grid[y][x]
 
     def set_cell(self, x, y, value):
-        if x < 0 or y < 0:
-            raise BoardIndexError("x and y positions must be zero or greater")
-        if y >= len(self.grid):
-            raise BoardIndexError("y position is out of range")
-        if x >= len(self.grid[y]):
-            raise BoardIndexError("x position is out of range")
+        self._check_bounds(x, y)
         self.grid[y][x] = value
